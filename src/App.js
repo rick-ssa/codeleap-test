0f29424813b69ed0525cfeab36d5c1517aa40a1d
rick-ssa <rick_ssa@hotmail.com>
2022-03-24 22:36:18 -0300

    made login page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 
 import './App.css';
-import ActionButton from './components/ActionButton';
-import Dialog from './components/Dialog';
-import InputText from './components/InputText';
-import Post from './components/Post';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <Dialog 
-        title = 'Welcome to CodeLeap network!'
-        buttons = {[
-          {text:'enter', active:true, simple: false, onClick:()=>alert('hello'), marginRight: '16px'},
-          {text:'sair', active:true, simple: false, onClick:()=>alert('hi')},
-        ]}
-      >
-        <InputText title={'Title'} placeholder='Title' value='Rick'/>
-        <InputText title={'Title'} placeholder='Title' value='Rick' multline/>
-      </Dialog>
-      
-      <p/>
-      <p/>
-      <p/>
-      <Post 
-        title={'This is a test'} 
-        text={'loren ipsum kljfkld jkl fjdskl jfklds jflkds  jfklds jflk dsjflk dsjfkl sdjlkf dsjlkf jsdl jfsdl fjksld fjlkds jfdslk jflsdk j'}  
-        creator={'@test'} 
-        time={'25 min ago'}
-      />
+      <Login />
     </div>
   );
 }
---
+++ src/pages/Login/index.js
@@ -0,0 +1,22 @@
+import Dialog from '../../components/Dialog'
+import InputText from '../../components/InputText'
+import './styles.css'
+
+
+const Login = () => {
+    return (
+        <div className='login'>
+            <Dialog
+                title='Welcome to CodeLeap network!'
+                buttons={[{text:'enter', active: true, simple: false, onClick:''}]}
+            >
+                <InputText 
+                    title='Please enter your username'
+                    placeholder='John Doe'
+                />
+            </Dialog>
+        </div>
+    )
+}
+
+export default Login
