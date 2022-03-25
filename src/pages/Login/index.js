d2c5d7d8bfbbea2e24963ad5d513f7f92c7f69ea
rick-ssa <rick_ssa@hotmail.com>
2022-03-24 22:52:20 -0300

    set enable/disable feature for the activebutton in login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,28 @@
+import { useState } from 'react'
 import Dialog from '../../components/Dialog'
 import InputText from '../../components/InputText'
 import './styles.css'
 
 
 const Login = () => {
+    const [text, setText] = useState('')
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+
     return (
         <div className='login'>
             <Dialog
                 width='500px'
                 title='Welcome to CodeLeap network!'
-                buttons={[{text:'enter', active: true, simple: false, onClick:''}]}
+                buttons={[{text:'enter', active: text !== '', simple: false, onClick:''}]}
             >
                 <InputText 
                     title='Please enter your username'
                     placeholder='John Doe'
+                    onChange =  {handleOnChange}
+                    value = {text}
                 />
             </Dialog>
         </div>
