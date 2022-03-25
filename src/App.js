8cbcfe587af027fa2195d905cb2f54d2cc40108f
rick-ssa <rick_ssa@hotmail.com>
2022-03-24 21:43:36 -0300

    made dialog component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 
 import './App.css';
 import ActionButton from './components/ActionButton';
+import Dialog from './components/Dialog';
 import InputText from './components/InputText';
 import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
-      <InputText title={'Title'} placeholder='Title' value='Rick'/>
-      <InputText title={'Title'} placeholder='Title' value='Rick' multline/>
-      <ActionButton text={'test'} active simple onClick={()=>alert('works')}/>
+      <Dialog 
+        title = 'Welcome to CodeLeap network!'
+        buttons = {[
+          {text:'enter', active:true, simple: false, onClick:()=>alert('hello')},
+          {text:'sair', active:true, simple: false, onClick:()=>alert('hi')},
+        ]}
+      >
+        <InputText title={'Title'} placeholder='Title' value='Rick'/>
+        <InputText title={'Title'} placeholder='Title' value='Rick' multline/>
+      </Dialog>
+      
+      <p/>
+      <p/>
+      <p/>
       <Post 
         title={'This is a test'} 
         text={'loren ipsum kljfkld jkl fjdskl jfklds jflkds  jfklds jflk dsjflk dsjfkl sdjlkf dsjlkf jsdl jfsdl fjksld fjlkds jfdslk jflsdk j'}  
---
+++ src/components/Dialog/index.js
@@ -0,0 +1,30 @@
+import ActionButton from '../ActionButton'
+import './styles.css'
+
+const Dialog = ({title, children, buttons}) => {
+    return (
+        <div className="dialog">
+            <h2 className="dialog__title">
+                {title}
+            </h2>
+            <div className="dialog__content">
+                {children}
+            </div>
+            <div className="dialog__buttons">
+                {buttons  && buttons.map((button, index)=>{
+                    return (
+                        <ActionButton 
+                            key = {index}
+                            text={button.text}
+                            active={button.active}
+                            simple={button.simple}
+                            onClick={button.onClick}
+                        />
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+export default Dialog
