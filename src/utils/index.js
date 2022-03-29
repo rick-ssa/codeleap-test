7854866fdae7345904155119e7b07ffcdc5ef14e
rick-ssa <rick_ssa@hotmail.com>
2022-03-29 18:49:09 -0300

    fixed bug

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,22 +6,31 @@ const transformTime = date => {
     const day = hour * 24
     const week = day * 7
 
-    switch(milisecondsPassed) {
-        case milisecondsPassed / minute < 0:
-            return 'now'
-        case milisecondsPassed / minute < 60:
-            const minutePassed = Math.floor(milisecondsPassed / minute)
-            return `${minutePassed} minute${minutePassed > 1 ? s : ''} ago`
-        case milisecondsPassed / hour < 24:
-            const hourPassed = Math.floor(milisecondsPassed / hour)
-            return `${hourPassed} hour${hourPassed > 1 ? s : ''} ago`
-        case milisecondsPassed / day < 7:
-            const dayPassed = Math.floor(milisecondsPassed / day)
-            return `${dayPassed} day${dayPassed > 1 ? s : ''} ago`
-        default:
-            const weekPassed = Math.floor(milisecondsPassed / week)
-            return `${weekPassed} week${weekPassed > 1 ? s : ''} ago`
+    const minutePassed = Math.floor(milisecondsPassed / minute)
+    const hourPassed = Math.floor(milisecondsPassed / hour)
+    const dayPassed = Math.floor(milisecondsPassed / day)
+    const weekPassed = Math.floor(milisecondsPassed / week)
+
+    console.log(milisecondsPassed)
+
+    if(minutePassed <= 0)  {
+        return 'now'
     }
+
+    if(minutePassed < 60) {
+        return `${minutePassed} minute${minutePassed > 1 ? 's' : ''} ago`
+    }
+
+    if(hourPassed < 24 ) {
+        return `${hourPassed} hour${hourPassed > 1 ? 's' : ''} ago`
+    }
+
+    if(dayPassed < 7) {
+        return `${dayPassed} day${dayPassed > 1 ? 's' : ''} ago`
+    }
+
+    return `${weekPassed} week${weekPassed > 1 ? 's' : ''} ago`
+
 }
 
 export {transformTime}
