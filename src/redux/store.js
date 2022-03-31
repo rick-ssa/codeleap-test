ef0cf05e4de9021e39ae6ee886094efe29de74d3
rick-ssa <rick_ssa@hotmail.com>
2022-03-30 21:13:01 -0300

    persisting state in the localstorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,15 @@ import {createStore} from 'redux'
 
 import rootReducer from './reducers/rootReducer'
 
-const store = createStore(rootReducer)
+let persistedState = JSON.parse(localStorage.getItem('state'))
+persistedState = {...persistedState,posts:persistedState.posts.map(
+    post => ({...post, created_datetime: new Date(post.created_datetime)})
+)}
+
+const store = createStore(rootReducer, persistedState ? persistedState : undefined)
+
+store.subscribe(()=>{
+    localStorage.setItem('state',JSON.stringify(store.getState()))
+})
 
 export default store
