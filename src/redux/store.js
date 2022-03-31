94649c6e1380864fdd0170e265c69f1951e18b6a
rick-ssa <rick_ssa@hotmail.com>
2022-03-31 20:26:17 -0300

    changed persistedState to const

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,7 @@ import {createStore} from 'redux'
 
 import rootReducer from './reducers/rootReducer'
 
-let persistedState = JSON.parse(localStorage.getItem('state'))
-persistedState = {...persistedState,posts:persistedState.posts.map(
-    post => ({...post, created_datetime: new Date(post.created_datetime)})
-)}
+const persistedState = JSON.parse(localStorage.getItem('state'))
 
 const store = createStore(rootReducer, persistedState ? persistedState : undefined)
 
