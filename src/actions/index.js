ed4f5d8d1e6003a790a457734ac4886ac62a7418
rick-ssa <rick_ssa@hotmail.com>
2022-03-31 21:51:36 -0300

    deleted some unecessary resources

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,4 @@
 const SINGUP = 'USER/SINGUP'
-const ADDED_POST = 'POST/ADDED_POST'
-const DELETED_POST = 'POST/DELETED_POST'
-const EDITED_POST = 'POST/EDITED_POST'
 const FETCHED_POST = 'POST/FETCHED_POST'
 
 const singupAction = (user) => ({
@@ -9,30 +6,10 @@ const singupAction = (user) => ({
     payload: {user}
 })
 
-const addPostAction = (newPost) => {
-    const created_datetime = new Date()
-    const id = created_datetime.getTime()
-    return ({
-        type: ADDED_POST,
-        payload: {
-            newPost: {...newPost, id, created_datetime}
-        }
-    })
-}
-
-const deletePostAction = (postId) =>({
-    type: DELETED_POST,
-    payload: {postId}
-})
-
-const editPostAction = (newPost) => ({
-    type: EDITED_POST,
-    payload: {newPost}
-})
 
 const fetchPost = (posts) => ({
     type: FETCHED_POST,
     payload: {posts}
 })
 
-export {singupAction, addPostAction, deletePostAction, editPostAction, fetchPost, SINGUP, ADDED_POST, DELETED_POST, EDITED_POST, FETCHED_POST}
+export {singupAction, fetchPost, SINGUP, FETCHED_POST}
--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -2,18 +2,6 @@ import {ADDED_POST, DELETED_POST, EDITED_POST, FETCHED_POST} from '../../actions
 
 const postReducer = (state = [], action) => {
     switch (action.type) {
-        case ADDED_POST: 
-            return [...state, action.payload.newPost]
-        
-        case DELETED_POST: 
-            return state.filter(post => post.id !== action.payload.postId)
-
-        case EDITED_POST: 
-            
-            return [
-                ...state.filter(post => post.id !== action.payload.newPost.id),
-                action.payload.newPost
-            ]
         case FETCHED_POST:
             return action.payload.posts
         default: 
